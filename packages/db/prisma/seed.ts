e90c6145a94cfb569f18a9d983231f5fe5961cbd
Mehisa Nanda <nandamehisa@gmail.com>
2025-08-19 14:10:02 +0700

    feat(ui): Change DB

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -3,146 +3,165 @@
  *
  * @link https://www.prisma.io/docs/guides/database/seed-database
  */
-import { Prisma, PrismaClient, Role } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
 import argon2 from 'argon2'
 
 const prisma = new PrismaClient()
-async function user() {
-  const roleData = [{ name: 'user' }]
-  const roles: Role[] = []
-  for (const data of roleData) {
-    const role = await prisma.role.upsert({
-      create: data,
-      update: data,
-      where: {
-        name: data.name,
-      },
-    })
-    roles.push(role)
-  }
-  const role = roles[0]?.id
-  if (!role) throw new Error('Role not found')
-  const userData: Prisma.UserUncheckedCreateInput[] = [
+async function student() {
+  const studentData: Prisma.StudentUncheckedCreateInput[] = [
     {
       name: 'Agung Suprianto',
       identity: '32567890',
       password: await argon2.hash('12071998'),
-      roleId: roles.find((role) => role.name === 'user')?.id || role,
     },
     {
       name: 'Budi Nugroho',
       identity: '87654321',
       password: await argon2.hash('15071998'),
-      roleId: roles.find((role) => role.name === 'user')?.id || role,
     },
     {
       name: 'Citra Halim',
       identity: '23456789',
       password: await argon2.hash('08122000'),
-      roleId: roles.find((role) => role.name === 'user')?.id || role,
     },
     {
       name: 'Dewi Siregar',
       identity: '34567890',
       password: await argon2.hash('22052002'),
-      roleId: roles.find((role) => role.name === 'user')?.id || role,
     },
     {
       name: 'Eko Prabowo',
       identity: '45678901',
       password: await argon2.hash('30111997'),
-      roleId: roles.find((role) => role.name === 'user')?.id || role,
     },
     {
       name: 'Fajar Ginting',
       identity: '56789012',
       password: await argon2.hash('14042003'),
-      roleId: roles.find((role) => role.name === 'user')?.id || role,
     },
     {
       name: 'Gita Kusuma',
       identity: '67890123',
       password: await argon2.hash('09081999'),
-      roleId: roles.find((role) => role.name === 'user')?.id || role,
     },
     {
       name: 'Hadi Purnama',
       identity: '78901234',
       password: await argon2.hash('27122001'),
-      roleId: roles.find((role) => role.name === 'user')?.id || role,
     },
     {
       name: 'Indra Wijaya',
       identity: '89012345',
       password: await argon2.hash('18061996'),
-      roleId: roles.find((role) => role.name === 'user')?.id || role,
     },
     {
       name: 'Joko Santoso',
       identity: '90123456',
       password: await argon2.hash('05101995'),
-      roleId: roles.find((role) => role.name === 'user')?.id || role,
     },
     {
       name: 'Ahmad Sihombing',
       identity: '12345678',
       password: await argon2.hash('29032004'),
-      roleId: roles.find((role) => role.name === 'user')?.id || role,
     },
   ]
 
-  for (const user of userData) {
-    await upsertUser(user)
+  for (const student of studentData) {
+    await upsertUser(student)
   }
 }
-async function upsertUser(userData: Prisma.UserUncheckedCreateInput) {
-  await prisma.user.upsert({
-    create: userData,
-    update: userData,
-    where: {
-      identity: userData.identity,
-    },
+async function upsertUser(studentData: Prisma.StudentUncheckedCreateInput) {
+  await prisma.student.upsert({
+    create: studentData,
+    update: studentData,
+    where: { identity: studentData.identity },
   })
 }
 
+const vocationData = [
+  {
+    key: 'ips',
+    name: 'Ilmu Pengetahuan Sosial',
+    subjects: [
+      'Pendidikan Agama Islam dan Budi Pekerti',
+      'Pendidikan Pancasila dan Kewarganegaraan',
+      'Bahasa Indonesia',
+      'Matematika',
+      'Sejarah Indonesia',
+      'Bahasa Inggris',
+      'Seni Budaya',
+      'Pendidikan Jasmani, Olahraga dan Kesehatan',
+      'Prakarya dan Kewirausahaan',
+      'Muatan Lokal Bahasa Daerah',
+      'Sejarah',
+      'Ekonomi',
+      'Geografi',
+      'Sosiologi',
+      'Bahasa Arab',
+      'Kecerdasan Umum',
+      'Pemahaman',
+      'Kemampuan Bahasa',
+      'Kemampuan Angka',
+      'Kemampuan Analisis',
+      'Kreativitas',
+      'Daya Ingat',
+      'Kemampuan Daya Bayang Ruang',
+      'Dorongan Berprestasi',
+      'Konsentrasi',
+      'Ketelitian',
+      'Kecepatan',
+      'Kepercayaan Diri',
+      'Stabilitas Emosi',
+      'Hubungan Antar Pribadi',
+      'Penyesuaian Diri',
+      'Kemandirian',
+    ],
+  },
+  {
+    key: 'ipa',
+    name: 'Ilmu Pengetahuan Alam',
+    subjects: [
+      'Pendidikan Agama Islam dan Budi Pekerti',
+      'Pendidikan Pancasila dan Kewarganegaraan',
+      'Bahasa Indonesia',
+      'Matematika',
+      'Sejarah Indonesia',
+      'Bahasa Inggris',
+      'Seni Budaya',
+      'Pendidikan Jasmani, Olahraga dan Kesehatan',
+      'Prakarya dan Kewirausahaan',
+      'Muatan Lokal Bahasa Daerah',
+      'Matematika (Peminatan)',
+      'Biologi',
+      'Fisika',
+      'Kimia',
+      'Bahasa Arab',
+      'Kecerdasan Umum',
+      'Pemahaman',
+      'Kemampuan Bahasa',
+      'Kemampuan Angka',
+      'Kemampuan Analisis',
+      'Kreativitas',
+      'Daya Ingat',
+      'Kemampuan Daya Bayang Ruang',
+      'Dorongan Berprestasi',
+      'Konsentrasi',
+      'Ketelitian',
+      'Kecepatan',
+      'Kepercayaan Diri',
+      'Stabilitas Emosi',
+      'Hubungan Antar Pribadi',
+      'Penyesuaian Diri',
+      'Kemandirian',
+    ],
+  },
+]
+
 // Seeder for ScoreGroup
-async function scoreGroups() {
-  const groups = [
-    'Pendidikan Agama Islam dan Budi Pekerti',
-    'Pendidikan Pancasila dan Kewarganegaraan',
-    'Bahasa Indonesia',
-    'Matematika',
-    'Sejarah Indonesia',
-    'Bahasa Inggris',
-    'Seni Budaya',
-    'Pendidikan Jasmani, Olahraga dan Kesehatan',
-    'Prakarya dan Kewirausahaan',
-    'Muatan Lokal Bahasa Daerah',
-    'Matematika (Peminatan)',
-    'Biologi',
-    'Fisika',
-    'Kimia',
-    'Bahasa Arab',
-    'Kecerdasan Umum',
-    'Pemahaman',
-    'Kemampuan Bahasa',
-    'Kemampuan Angka',
-    'Kemampuan Analisis',
-    'Kreativitas',
-    'Daya Ingat',
-    'Kemampuan Daya Bayang Ruang',
-    'Dorongan Berprestasi',
-    'Konsentrasi',
-    'Ketelitian',
-    'Kecepatan',
-    'Kepercayaan Diri',
-    'Stabilitas Emosi',
-    'Hubungan Antar Pribadi',
-    'Penyesuaian Diri',
-    'Kemandirian',
-  ]
+async function subjects() {
+  const groups = vocationData.flatMap((vocation) => vocation.subjects)
   for (const name of groups) {
-    await prisma.scoreGroup.upsert({
+    await prisma.subject.upsert({
       where: { name },
       update: {},
       create: { name },
@@ -150,8 +169,27 @@ async function scoreGroups() {
   }
 }
 
+async function vocations() {
+  for (const vocation of vocationData) {
+    await prisma.vocation.upsert({
+      where: { key: vocation.key },
+      update: {},
+      create: {
+        key: vocation.key,
+        name: vocation.name,
+        subjects: {
+          connectOrCreate: vocation.subjects.map((subject) => ({
+            where: { name: subject },
+            create: { name: subject },
+          })),
+        },
+      },
+    })
+  }
+}
+
 async function main() {
-  const seeders: Promise<void>[] = [user(), scoreGroups()]
+  const seeders: Promise<void>[] = [student(), vocations()]
   await Promise.all(seeders)
 }
 
